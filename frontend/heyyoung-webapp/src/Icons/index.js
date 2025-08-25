269a9b01d980cbe8c6d7481b4590fcbf1db9f38c
redblackblossom <physicsdev1029@gmail.com>
2025-08-26 08:24:13 +0900

    [CHORE] 프론트엔드 코드 삭제

--- frontend/heyyoung-webapp/src/Icons/index.js
+++
@@ -1,4 +0,0 @@
-// src/Icons/index.js
-import bookmark from '@/Icons/svg/bookmark.svg';
-
-export const icons = { bookmark };
--- frontend/heyyoung-webapp/src/Page/NotFound.jsx
+++
@@ -1,1 +0,0 @@
-export default function NotFound(){ return <div style={{ padding:'24px 0' }}>페이지가 없어요.</div> }
