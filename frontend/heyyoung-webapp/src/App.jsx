a3c3897e4abe32992afb9fc3c65fb16b0dd7ca1d
Seo Ga Eun <seo323333@naver.com>
2025-08-26 20:36:24 +0900

    [MERGE] App.jsx 충돌 해결

--- frontend/heyyoung-webapp/src/App.jsx
+++ frontend/heyyoung-webapp/src/App.jsx
@@ -1,21 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect, Suspense, lazy } from 'react'
 import AppShell from './Components/layout/AppShell'
-import MainHomePage from './Page/MainHomePage'
-import NotificationPage from './Page/NotificationPage'
-import BenefitMainPage from './Page/BenefitMainPage'
-import EntireMenuPage from './Page/EntireMenuPage'
-import Payment from './Page/Payment'
-import NotFound from './Page/NotFound'
-// TODO: 새로운 제휴 페이지들 import 예정
-import PartnershipMainPage from './Page/PartnershipMainPage'
-// import PartnershipMapPage from './Page/PartnershipMapPage'
-import PartnershipListPage from './Page/PartnershipListPage'
-// import PartnershipDetailPage from './Page/PartnershipDetailPage'
-import PaymentPage from './Page/PaymentPage'
-import PaymentResultPage from './Page/PaymentResultPage'
 import PageTransition from './Components/layout/PageTransition'
-
 import './App.css'
 
 // React Native 브리지 초기화
@@ -27,10 +13,13 @@ const NotificationPage = lazy(() => import('./Page/NotificationPage'))
 const BenefitMainPage = lazy(() => import('./Page/BenefitMainPage'))
 const EntireMenuPage = lazy(() => import('./Page/EntireMenuPage'))
 const Payment = lazy(() => import('./Page/Payment'))
-const ButtonExamplePage = lazy(() => import('./Page/ButtonExamplePage'))
 const MapView = lazy(() => import('./Page/MapView'))
 const StoreDetailPage = lazy(() => import('./Page/StoreDetailPage'))
 const NotFound = lazy(() => import('./Page/NotFound'))
+const PartnershipMainPage = lazy(() => import('./Page/PartnershipMainPage'))
+const PartnershipListPage = lazy(() => import('./Page/PartnershipListPage'))
+const PaymentPage = lazy(() => import('./Page/PaymentPage'))
+const PaymentResultPage = lazy(() => import('./Page/PaymentResultPage'))
 
 // 로딩 컴포넌트
 const LoadingSpinner = () => (
@@ -63,9 +52,12 @@ function ScrollToTop() {
         '/benefit-main': 'benefitMain',
         '/entire-menu': 'entireMenu',
         '/pay': 'pay',
-        '/button-examples': 'buttonExamples',
         '/benefit-map': 'benefitMap',
-        '/store-detail': 'storeDetail'
+        '/store-detail': 'storeDetail',
+        '/partnership': 'partnership',
+        '/partnership/list': 'partnershipList',
+        '/payment': 'payment',
+        '/payment-result': 'paymentResult'
       };
       
       const currentRoute = routeMap[pathname] || 'home';
@@ -82,25 +74,21 @@ function App() {
       <ScrollToTop />
       <AppShell>
         <PageTransition>
-        <Suspense fallback={<LoadingSpinner />}>
-
-        <Routes>
-          <Route path="/" element={<MainHomePage />} />
-          <Route path="/notifications" element={<NotificationPage />} />
-          <Route path="/benefit-main" element={<BenefitMainPage />} />
-          <Route path="/entire-menu" element={<EntireMenuPage />} />
-          <Route path="/pay" element={<Payment />} />
-          {/* TODO: 새로운 제휴 페이지 라우트 예정 */}
-          <Route path="/partnership" element={<PartnershipMainPage />} />
-          {/* <Route path="/partnership/map" element={<PartnershipMapPage />} /> */}
-          <Route path="/partnership/list" element={<PartnershipListPage />} />
-          {/* <Route path="/partnership/detail/:id" element={<PartnershipDetailPage />} /> */}
-          <Route path="/payment" element={<PaymentPage />} />
-            <Route path="/payment-result" element={<PaymentResultPage />} />
-            <Route path="/benefit-map" element={<MapView />} />
-            <Route path="/store-detail" element={<StoreDetailPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Suspense fallback={<LoadingSpinner />}>
+            <Routes>
+              <Route path="/" element={<MainHomePage />} />
+              <Route path="/notifications" element={<NotificationPage />} />
+              <Route path="/benefit-main" element={<BenefitMainPage />} />
+              <Route path="/entire-menu" element={<EntireMenuPage />} />
+              <Route path="/pay" element={<Payment />} />
+              <Route path="/benefit-map" element={<MapView />} />
+              <Route path="/store-detail" element={<StoreDetailPage />} />
+              <Route path="/partnership" element={<PartnershipMainPage />} />
+              <Route path="/partnership/list" element={<PartnershipListPage />} />
+              <Route path="/payment" element={<PaymentPage />} />
+              <Route path="/payment-result" element={<PaymentResultPage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </Suspense>
         </PageTransition>
       </AppShell>
