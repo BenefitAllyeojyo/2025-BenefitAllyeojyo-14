3e016c921cc6e8c57764158b010398afe10a910d
Seo Ga Eun <seo323333@naver.com>
2025-08-27 07:28:18 +0900

    [FEAT] 제휴 미니 컴포넌트 추가

--- frontend/heyyoung-webapp/src/Components/molecules/PartnershipCarouselBanner/index.jsx
+++ frontend/heyyoung-webapp/src/Components/molecules/PartnershipCarouselBanner/index.jsx
@@ -1,1 +1,4 @@
 export { default } from './PartnershipCarouselBanner';
+export { default as PartnershipCard } from './PartnershipCard';
+export { default as PartnershipDetailCard } from './PartnershipDetailCard';
+export { default as CarouselControls } from './CarouselControls';
