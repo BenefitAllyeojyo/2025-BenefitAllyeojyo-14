6cca6b7eb57ccdf4b8b637936b242962649506af
Seo Ga Eun <seo323333@naver.com>
2025-08-30 00:16:29 +0900

    [FIX] 버튼 클릭 수정

--- frontend/heyyoung-webapp/src/Components/atoms/Button/RegularButton.jsx
+++ frontend/heyyoung-webapp/src/Components/atoms/Button/RegularButton.jsx
@@ -3,15 +3,34 @@ import styles from './RegularButton.module.css'
 export default function RegularButton({
   label, onClick, disabled = false, white = false
 }) {
+  const handleClick = (e) => {
+    console.log('RegularButton 클릭됨:', label);
+    if (onClick) {
+      onClick(e);
+    }
+  };
+
+  const handleTouchStart = (e) => {
+    console.log('RegularButton 터치 시작:', label);
+  };
+
+  const handleTouchEnd = (e) => {
+    console.log('RegularButton 터치 종료:', label);
+    // 터치 종료 시에도 클릭 이벤트 발생
+    handleClick(e);
+  };
+
   return (
     <button
       type="button"
-      onClick={onClick}
+      onClick={handleClick}
+      onTouchStart={handleTouchStart}
+      onTouchEnd={handleTouchEnd}
       disabled={disabled}
       className={white ? styles.whiteButton : styles.regularButton}
-      label = {label}
+      label={label}
     >
-      <span className={ styles.label}>{label}</span>
+      <span className={styles.label}>{label}</span>
     </button>
   )
 }
