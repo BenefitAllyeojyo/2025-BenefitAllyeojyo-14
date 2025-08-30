8757c09fa92dc40014b9934f6d000f1993277da0
redblackblossom <physicsdev1029@gmail.com>
2025-08-30 10:15:36 +0900

    [FEAT] 시간 동기화 코드 추가

--- backend/core-bank/src/main/java/com/heyoung/HeyoungCoreBankApplication.java
+++ backend/core-bank/src/main/java/com/heyoung/HeyoungCoreBankApplication.java
@@ -1,14 +1,24 @@
 package com.heyoung;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
 public class HeyoungCoreBankApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HeyoungCoreBankApplication.class, args);
 	}
